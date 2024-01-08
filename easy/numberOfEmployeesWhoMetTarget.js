0ee7810d9d59e71652145c46e77c3de7cf313dac
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2024-01-08 21:11:04 +0400

    Add numberOfEmployeesWhoMetTarget.js

---
+++ easy/numberOfEmployeesWhoMetTarget.js
@@ -0,0 +1,11 @@
+const numberOfEmployeesWhoMetTarget = function(hours, target) {
+    let employees = 0;
+
+    for (let i = 0; i < hours.length; i++) {
+        if (hours[i] >= target) {
+            employees++;
+        }
+    }
+
+    return employees;
+};
