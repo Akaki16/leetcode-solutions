a0f1f11284ce76544b7ea6dac5826a1fc8f1b406
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2024-02-06 19:04:39 +0400

    Update binarySearch.js

--- easy/binarySearch.js
+++ easy/binarySearch.js
@@ -1,15 +1,15 @@
 "use strict";
 
 const binarySearch = function (nums, target) {
-  let low = 0;
+  let left = 0;
   let right = nums.length - 1;
 
-  while (low <= right) {
-    let mid = Math.floor((low + right) / 2);
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
     if (nums[mid] === target) {
       return mid;
     } else if (nums[mid] < target) {
-      low = mid + 1;
+      left = mid + 1;
     } else {
       right = mid - 1;
     }
