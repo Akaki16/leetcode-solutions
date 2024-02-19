46ac77579c33adec5b0eb67ebfe0fcac5e5d7697
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2024-02-19 17:10:46 +0400

    Add targetIndices.js

--- easy/targetIndices.js
+++ easy/targetIndices.js
@@ -8,8 +8,8 @@ const targetIndices = function (nums, target) {
   const sortedNums = nums.sort((a, b) => a - b);
   const result = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target) {
+  for (let i = 0; i < sortedNums.length; i++) {
+    if (sortedNums[i] === target) {
       result.push(i);
     }
   }
